176cc847ed813973736a231de5d73ffbd6384c01
akazukin5151 <tsuiyikching@protonmail.com>
2022-07-14 01:48:19 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# get-paths
+
+This is a simple script that looks at the git status of the current directory of the program, and prints out the paths of all files with unstaged changes. Also accepts a single command line argument to strip a prefixed string off every path
---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use std::process::Command;
+use std::collections::HashMap;
+use std::env;
+
+fn main() {
+    let keys = HashMap::from([
+            (" M ", "modified"),
+            (" A ", "added"),
+            (" D ", "deleted"),
+            (" R ", "renamed"),
+            (" C ", "copied"),
+            ("?? ", "new file"),
+    ]);
+
+    let out = Command::new("git").args(["status", "--porcelain=v1"]).output().unwrap();
+    let output = String::from_utf8(out.stdout).unwrap();
+
+    for line in output.split('\n') {
+        for (key, desc) in &keys {
+            if line.starts_with(key) {
+                let maybe_stripped = line.strip_prefix(key);
+                let stripped = maybe_stripped.unwrap_or(line);
+
+                let prefix_to_strip = env::args().nth(1).unwrap();
+                dbg!(&prefix_to_strip);
+
+                println!("{}", desc);
+                println!(
+                    "{}\n",
+                    stripped.strip_prefix(&prefix_to_strip).unwrap_or(stripped)
+                );
+            }
+        }
+    }
+}
